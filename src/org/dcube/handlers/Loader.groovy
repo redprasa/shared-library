e920ddf9a936ba9e5e19a740f019109496c340bb
redprasa <39630520+redprasa@users.noreply.github.com>
2021-01-03 14:24:42 +0530

    svksfnvkfv
    fn vkfvnkfv

---
+++ src/org/dcube/handlers/Loader.groovy
@@ -0,0 +1,6 @@
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import hudson.util.Secret
+import org.yaml.snakeyaml.Yaml
+import groovy.json.JsonBuilder
+return this
